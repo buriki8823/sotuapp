a11357261e25d710887647145e861baf9fa3983e
buriki8823 <buriki2_8823@yahoo.co.jp>
2025-09-21 20:23:06 +0900

    ログイン画面の作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,18 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:load", () => {
+  const bg = document.getElementById("login-background");
+  const card = document.querySelector(".login-card");
+
+  if (card) {
+    card.classList.remove("show"); // 初期状態をリセット
+  }
+
+  if (bg && card) {
+    bg.addEventListener("click", () => {
+      card.classList.add("show");
+    });
+  }
+});
