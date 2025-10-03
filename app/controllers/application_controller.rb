29985599014a5dc835809414d258485c45b85c04
buriki8823 <buriki2_8823@yahoo.co.jp>
2025-10-03 21:05:29 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  def after_sign_in_path_for(resource)
+    authenticated_root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
