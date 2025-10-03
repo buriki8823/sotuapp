de564dfa0286afcf25443cb2152bc4948479d849
buriki8823 <buriki2_8823@yahoo.co.jp>
2025-10-03 20:48:59 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 Rails.application.routes.draw do
-  get "bookmarks/index"
-  get "posts/index"
-  root to: "home#index"
-  get "home/index"
-  get 'mypage', to: 'users#mypage'
-  resources :posts, only: [:index, :show, :new, :create]
-  resources :bookmarks, only: [:index]
-  resources :users, only: [:show, :update] do
-   collection do
-     get :mypage
-     patch :reset_mypage_background
-     patch :reset_window_background
-   end
-  end
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
 
+  authenticated :user do
+    root to: "home#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "devise/sessions#new", as: :unauthenticated_root
+  end
+
+  get 'mypage', to: 'users#mypage'
+  resources :posts, only: [:index, :show, :new, :create]
+  resources :bookmarks, only: [:index]
+  resources :users, only: [:show, :update] do
+    collection do
+      get :mypage
+      patch :reset_mypage_background
+      patch :reset_window_background
+    end
+  end
+
   # PWA関連
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # ユーザー情報の表示・編集を追加したい場合は以下を使う
-  # resources :users, only: [:show, :edit, :update]
 end
