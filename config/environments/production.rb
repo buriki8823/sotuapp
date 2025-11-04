c1b0e7666406baab69c3f104ee05729be486bbf1
buriki8823 <buriki2_8823@yahoo.co.jp>
2025-11-04 20:08:06 +0900

    微修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,11 @@ Rails.application.configure do
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_options = {
+  from: 'sk.typhoon999@gmail.com' # ← SendGridで認証済みのアドレスに変更
+}
+
   config.action_mailer.default_url_options = { host: 'sotuapp-v2.fly.dev', protocol: 'https' }
 
   config.action_mailer.delivery_method = :smtp
@@ -91,7 +96,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
