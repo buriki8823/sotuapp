8547fed3bf082029252e6618ea20da74b90089b4
buriki8823 <buriki2_8823@yahoo.co.jp>
2025-11-10 00:04:59 +0900

    ブックマーク解除ボタンとコメントの非同期化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,13 +4,17 @@ class BookmarksController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     current_user.bookmarked_posts << post unless current_user.bookmarked_posts.exists?(post.id)
-    redirect_to post_path(post), notice: "ブックマークしました"
+    head :ok
   end
 
   def destroy
     post = Post.find(params[:post_id])
-    current_user.bookmarked_posts.destroy(post)
-    redirect_to post_path(post), notice: "ブックマークを解除しました"
+    current_user.bookmarks.find_by(post: post)&.destroy
+
+    respond_to do |format|
+      format.html { redirect_to bookmarks_path, notice: "ブックマークを解除しました" }
+      format.json { head :no_content }
+    end
   end
 
   def index
