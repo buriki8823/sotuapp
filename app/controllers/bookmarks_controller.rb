89be845c8f442bc7cde94be0554ee298ba5dea1e
buriki8823 <buriki2_8823@yahoo.co.jp>
2025-11-11 23:45:17 +0900

    非同期修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,23 +1,32 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: [:create, :destroy]
 
   def create
-    post = Post.find(params[:post_id])
-    current_user.bookmarked_posts << post unless current_user.bookmarked_posts.exists?(post.id)
-    head :ok
+    current_user.bookmarked_posts << @post unless current_user.bookmarked_posts.exists?(@post.id)
+
+    respond_to do |format|
+      format.html { redirect_to @post, notice: "ブックマークしました" }
+      format.json { render json: { status: "bookmarked" }, status: :created }
+    end
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    current_user.bookmarks.find_by(post: post)&.destroy
+    current_user.bookmarks.find_by(post: @post)&.destroy
 
     respond_to do |format|
       format.html { redirect_to bookmarks_path, notice: "ブックマークを解除しました" }
-      format.json { head :no_content }
+      format.json { render json: { status: "unbookmarked" }, status: :ok }
     end
   end
 
   def index
     @bookmarked_posts = current_user.bookmarked_posts.page(params[:page]).per(9)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
